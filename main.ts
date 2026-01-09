51c8b23ce14584b1ceeac325ab97f493cf20f9f2
LassazVegaz <lasindunuwanga@gmail.com>
2026-01-10 05:05:17 +0800

    polynomial regression way

--- main.ts
+++ main.ts
@@ -1,155 +1,96 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-const FILE_WEIGHTS = path.join("data", "weights.json");
+type Params = { w1: number; w2: number; b: number };
+
+const FILE_PARAMETERS = path.join("data", "params.json");
 const FILE_TRAINING_DATA = path.join("data", "trainingData.json");
 
-/**
- * Neural Network with one hidden layer
- * Hidden layer has 2 neurons
- * One output neuron and 1 input
- */
-
-type Parameters = {
-  nInW: number[];
-  nInB: number[];
-  nOutW: number[];
-  nOutB: number[];
-  b: number;
-};
-
-type ForwardResults = {
-  out: number;
-  nY: number[];
-  nX: number[];
-};
-
-const getTrainingData = () => {
-  if (fs.existsSync(FILE_TRAINING_DATA)) {
-    const data = fs.readFileSync(FILE_TRAINING_DATA, "utf-8");
-    return JSON.parse(data) as number[];
+const createData = () => {
+  const nums: number[] = [];
+  for (let i = 0; i < 1000; i++) {
+    nums.push(Math.random() * 100);
   }
 
-  console.log("Generating training data...");
-  const trainingData: number[] = [];
-  for (let i = 0; i <= 10000; i++) {
-    const x = Math.random();
-    trainingData.push(x);
-  }
-  fs.writeFileSync(FILE_TRAINING_DATA, JSON.stringify(trainingData));
-  console.log("Training data generated.");
-  return trainingData;
+  fs.writeFileSync(FILE_TRAINING_DATA, JSON.stringify(nums), {
+    encoding: "utf8",
+  });
 };
 
-const saveParameters = (p: Parameters) => {
-  fs.writeFileSync(FILE_WEIGHTS, JSON.stringify(p));
+const getData = () => {
+  if (!fs.existsSync(FILE_TRAINING_DATA)) createData();
+
+  const str = fs.readFileSync(FILE_TRAINING_DATA, { encoding: "utf8" });
+  const nums = JSON.parse(str) as number[];
+  return nums;
 };
 
-const loadParameters = (): Parameters => {
-  if (fs.existsSync(FILE_WEIGHTS)) {
-    const data = fs.readFileSync(FILE_WEIGHTS, "utf-8");
-    const json = JSON.parse(data) as Parameters;
-    console.log("Loaded weights:", json);
-    return json;
-  }
-  return {
-    nInW: [Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05],
-    nInB: [Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05],
-    nOutW: [Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05],
-    nOutB: [Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05],
-    b: Math.random() * 0.1 - 0.05,
-  };
+const saveParams = (p: Params) => {
+  fs.writeFileSync(FILE_PARAMETERS, JSON.stringify(p), {
+    encoding: "utf-8",
+  });
 };
 
-const f = (x: number) => 2 * x + x * x; // 2x + x^2
+const getParams = () => {
+  if (!fs.existsSync(FILE_PARAMETERS)) saveParams({ w1: 0, w2: 0, b: 0 });
 
-const neuronsCount = 2;
-const a = 1e-3;
-let p = loadParameters();
-
-const forward = (x: number, p: Parameters): ForwardResults => {
-  const nX: number[] = [];
-  const nY: number[] = [];
-  let out = 0;
-
-  for (let i = 0; i < neuronsCount; i++) {
-    nX[i] = p.nInW[i] * x + p.nInB[i];
-    nY[i] = Math.max(0, nX[i]);
-    const nOut = p.nOutW[i] * nY[i] + p.nOutB[i];
-    out += nOut;
-  }
-
-  out += p.b;
-
-  return { out, nY, nX };
+  const json = fs.readFileSync(FILE_PARAMETERS, { encoding: "utf-8" });
+  return JSON.parse(json) as Params;
 };
 
-const reLuDerivative = (x: number) => (x > 0 ? 1 : 0);
+// y = x^2 + 2x + 5
+const f = (x: number) => x ** 2 + 2 * x + 5;
 
-const backward = (
-  x: number,
-  p: Parameters,
-  e: number,
-  f: ForwardResults
-): Parameters => {
-  // o = n1 + n2 + b
-  // j = 0.5 * (y - o)^2
-  // dj/dnOutB = dj/dout . dout/dnOutB = dj/dout
-  // dj/dx = dj/dout . dout/dnOut . dnOut/dnY . dnY/dnX . dnX/dx
-  // dj/dnOutW = dj/dout . dout/dnOut . dnOut/dnOutW
-  // dj/dnInW = dj/dout . dout/dnOut . dnOut/dnY . dnY/dnX . dnX/dnInW
-  // dj/dnInB = dj/dout . dout/dnOut . dnOut/dnY . dnY.dnX . dnX/dnInB
+const forward = (x: number, p: Params) => x ** 2 * p.w1 + x * p.w2 + p.b;
 
-  const nInW: number[] = [];
-  const nInB: number[] = [];
-  const nOutW: number[] = [];
-  const nOutB: number[] = [];
-  const dj_dout = -e;
+const main = () => {
+  const data = getData();
+  const max = Math.max(...data);
+  const a = 0.001;
 
-  for (let i = 0; i < neuronsCount; i++) {
-    const dj_dnX = -e * p.nOutW[i] * reLuDerivative(f.nX[i]);
-    nInW[i] = p.nInW[i] - a * dj_dnX * x;
-    nInB[i] = p.nInB[i] - a * dj_dnX;
-    nOutW[i] = p.nOutW[i] - a * -e * f.nY[i];
-    nOutB[i] = p.nOutB[i] - a * dj_dout;
-  }
+  let { w1, w2, b } = getParams();
 
-  return {
-    b: p.b - a * dj_dout,
-    nInW,
-    nInB,
-    nOutB,
-    nOutW,
-  };
-};
+  // x => w.x + b => y
 
-const train = (n = 300) => {
-  let l = 0;
-  let maxError = -Infinity;
-  const trainingData = getTrainingData();
+  // MSE = mean square error = 0.5 * sum of square errors / number of training data (n)
+  let MSE = 0;
+  for (let round = 0; round < 10000; round++) {
+    // dW = 0.5 * -xi * sum of errors / n
+    // dB = 0.5 * -sum of errors / n
 
-  for (let i = 0; i < n; i++) {
-    for (const x of trainingData) {
+    let dW1 = 0,
+      dW2 = 0,
+      dB = 0;
+
+    for (const _x of data) {
+      const x = _x / max;
+      const h = forward(x, { w1, w2, b });
       const y = f(x);
-      const _y = forward(x, p);
-      const e = y - _y.out;
-      p = backward(x, p, e, _y);
-      l = (y - _y.out) ** 2 / 2;
-
-      const absE = Math.abs(e);
-      if (absE > maxError) maxError = absE;
+      const e = y - h;
+      dW1 += -(x ** 2) * e;
+      dW2 += -x * e;
+      dB += -e;
+      MSE += e ** 2;
     }
+
+    const n = data.length;
+    MSE = (0.5 * MSE) / n;
+    w1 -= (dW1 / n) * a;
+    w2 -= (dW2 / n) * a;
+    b -= (dB / n) * a;
+
+    if (round % 1000 === 0)
+      console.log(`${round} -> ${MSE} : {${w1} ${w2} ${b}}`);
   }
 
-  return { l, maxError };
+  saveParams({ w1, w2, b });
+
+  console.log("Finished training.....");
+  console.log(`Last MSE -> ${MSE} : {${w1} ${w2} ${b}}`);
+  const testData = [1, 1026.9854, 101.01, 32];
+  for (const d of testData) {
+    console.log(`x = ${d}, y =${forward(d, { w1, w2, b })}`);
+  }
 };
 
-console.log("before", p);
-const res = train(1000);
-console.log("final l =", res.l, " maxError =", res.maxError);
-console.log("after", p);
-saveParameters(p);
-
-for (let x = 5; x <= 10; x++) {
-  console.log(`x: ${x}, f(x): ${f(x)}, h(x): ${forward(x, p).out}`);
-}
+main();
