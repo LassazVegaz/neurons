747114f5531a55bdd26c92a72f5d3b612e11b533
LassazVegaz <lasindunuwanga@gmail.com>
2026-02-14 15:20:01 +0800

    some comments GPT ones

--- main gpt.ts
+++ main gpt.ts
@@ -1,7 +1,7 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-import { Network, type Params } from "./lib/Neurone";
+import { Network, type Params } from "./lib/Neurone gpt";
 
 const FILE_PARAMETERS = path.join("data", "params.json");
 const FILE_TRAINING_DATA = path.join("data", "trainingData.json");
