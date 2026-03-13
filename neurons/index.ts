41febe4232265785ff505772d5d96477c12c7962
LassazVegaz <lasindunuwanga@gmail.com>
2026-03-14 04:20:51 +0800

    export Model and Norm params from Neurons index

--- neurons/index.ts
+++ neurons/index.ts
@@ -1,1 +1,7 @@
-export { Network, type TrainParams, type ModelParameters } from "./Neurone.js";
+export {
+  Network,
+  type TrainParams,
+  type ModelParameters,
+  type NormalizationParameters,
+  type Model,
+} from "./Neurone.js";
