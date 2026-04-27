f0fcac9a64b7ad3006e99230436ffe543ae0c2aa
LassazVegaz <lasindunuwanga@gmail.com>
2026-04-27 14:20:26 +0800

    test a little

--- dotnet/App/App.Console/Program.cs
+++ dotnet/App/App.Console/Program.cs
@@ -42,3 +42,14 @@ var network = new Network(new()
     f = f,
     normParams = model.normParams
 });
+
+network.Train(trainingData, model.thetas);
+
+await Storage.SaveModel(model);
+
+var testData = new double[] { 1, 2, 100, 102.3698, 0.258 };
+foreach (var x in testData)
+{
+    var p = network.Predict(x, model.thetas);
+    Console.WriteLine($"when x = {x}, y = {f(x)}, prediction = {p}");
+}
