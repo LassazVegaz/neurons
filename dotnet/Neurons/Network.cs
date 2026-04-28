840f03da71e928803bd8f59120f29f2c96991c19
LassazVegaz <lasindunuwanga@gmail.com>
2026-04-28 13:28:14 +0800

    backward func

--- dotnet/Neurons/Network.cs
+++ dotnet/Neurons/Network.cs
@@ -26,56 +26,12 @@ public class Network(NetworkParameters networkParams)
             {
                 // forward propagation
                 var fResult = H(x, t);
-                var predicted = fResult.a[^1][0];
-
-                // error signals comming from every neurone
-                var eSignal = new double[layers.Length][];
-                var E = -(f(x) - predicted);
-                eSignal[^1] = [E];
 
                 // backward propagation
-                // from the last layer to the first one
-                for (var b = layers.Length - 1; b >= 0; b--)
-                {
-                    var prevLayerI = b - 1; // prev layer index
-                    var gapI = b - 1; // gap index for thetas
-
-                    dT.b[gapI] = new double[layers[b]];
-                    dT.w[gapI] = new double[layers[b]][];
-
-                    // for every neurone in the current layer
-                    for (var c = 0; c < layers[b]; c++)
-                    {
-                        dT.w[gapI][c] = new double[layers[prevLayerI]];
-
-                        // accumulating partial derivative of bias
-                        dT.b[gapI][c] += eSignal[b][c];
-
-                        // for every neurone in the previous layer
-                        for (var d = 0; d < layers[prevLayerI]; d++)
-                        {
-                            // accumulating partial derivative of weights
-                            dT.w[gapI][c][d] += fResult.a[prevLayerI][d] * eSignal[b][c];
-                        }
-                    }
-
-                    // calculating error signals
-                    eSignal[prevLayerI] = new double[layers[prevLayerI]];
-
-                    // for every neurone in the previous layer
-                    for (var c = 0; c < layers[prevLayerI]; c++)
-                    {
-                        // for every neurone in the current layer
-                        for (var d = 0; d < layers[b]; d++)
-                        {
-                            eSignal[prevLayerI][c] +=
-                                DRelU(fResult.befA[prevLayerI][c]) * t.w[gapI][d][c] * eSignal[b][d];
-                        }
-                    }
-                }
+                Backward(t, dT, fResult);
             }
 
-            // update thetas
+            // update thetas using
             for (var b = 0; b < layers.Length - 2; b++)
             {
                 for (var c = 0; c < layers[b + 1]; c++)
@@ -140,6 +96,57 @@ public class Network(NetworkParameters networkParams)
         }
 
         return new() { a = a, befA = befA };
+    }
+
+    private void Backward(Thetas t, Thetas dT, ForwardResults fResult)
+    {
+        var x = fResult.a[0][0];
+        var predicted = fResult.a[^1][0];
+
+        // error signals comming from every neurone
+        var eSignal = new double[layers.Length][];
+        var E = -(f(x) - predicted);
+        eSignal[^1] = [E];
+
+        // from the last layer to the first one
+        for (var b = layers.Length - 1; b >= 0; b--)
+        {
+            var prevLayerI = b - 1; // prev layer index
+            var gapI = b - 1; // gap index for thetas
+
+            dT.b[gapI] = new double[layers[b]];
+            dT.w[gapI] = new double[layers[b]][];
+
+            // for every neurone in the current layer
+            for (var c = 0; c < layers[b]; c++)
+            {
+                dT.w[gapI][c] = new double[layers[prevLayerI]];
+
+                // accumulating partial derivative of bias
+                dT.b[gapI][c] += eSignal[b][c];
+
+                // for every neurone in the previous layer
+                for (var d = 0; d < layers[prevLayerI]; d++)
+                {
+                    // accumulating partial derivative of weights
+                    dT.w[gapI][c][d] += fResult.a[prevLayerI][d] * eSignal[b][c];
+                }
+            }
+
+            // calculating error signals
+            eSignal[prevLayerI] = new double[layers[prevLayerI]];
+
+            // for every neurone in the previous layer
+            for (var c = 0; c < layers[prevLayerI]; c++)
+            {
+                // for every neurone in the current layer
+                for (var d = 0; d < layers[b]; d++)
+                {
+                    eSignal[prevLayerI][c] +=
+                        DRelU(fResult.befA[prevLayerI][c]) * t.w[gapI][d][c] * eSignal[b][d];
+                }
+            }
+        }
     }
 
     private double Normalize(double x) => (x - normParams.mean) / normParams.standardDeviation;
