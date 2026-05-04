fd7cf5b837904081fd278ed9ba399ce727f92e4c
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-04 16:38:35 +0800

    workload shifted

---
+++ dotnet/App/App.Console/EssentialsBuilder.cs
@@ -0,0 +1,48 @@
+﻿namespace Neurons.App.Console;
+
+internal static class EssentialsBuilder
+{
+    public static async Task<Model> GetModel(int[] layers, double[] trainingData)
+    {
+        Model model;
+
+        if (Storage.ModelExists())
+        {
+            model = await Storage.GetModel()
+                ?? throw new Exception("Model is null!");
+        }
+        else
+        {
+            model = new()
+            {
+                thetas = ThetasInitializations.HeInitialization(layers),
+                normParams = new()
+                {
+                    mean = trainingData.Average(),
+                    standardDeviation = Tools.GetStandardDeviation(trainingData)
+                }
+            };
+            await Storage.SaveModel(model);
+        }
+
+        return model;
+    }
+
+    public static async Task<double[]> GetTrainingData()
+    {
+        double[] trainingData;
+
+        if (Storage.TrainingDataFileExists)
+        {
+            trainingData = await Storage.GetTrainingData()
+                ?? throw new Exception("Training data is null!");
+        }
+        else
+        {
+            trainingData = Tools.GenerateTrainingData();
+            await Storage.SaveTrainingData(trainingData);
+        }
+
+        return trainingData;
+    }
+}
--- dotnet/App/App.Console/Program.cs
+++ dotnet/App/App.Console/Program.cs
@@ -5,38 +5,9 @@ const int ITERATIONS_COUNT = 1000;
 
 var f = (double x) => x;
 int[] layers = [1, 1, 1];
-double[] trainingData;
-Model model;
 
-if (Storage.TrainingDataFileExists)
-{
-    trainingData = await Storage.GetTrainingData()
-        ?? throw new Exception("Training data is null!");
-}
-else
-{
-    trainingData = Tools.GenerateTrainingData();
-    await Storage.SaveTrainingData(trainingData);
-}
-
-if (Storage.ModelExists())
-{
-    model = await Storage.GetModel()
-        ?? throw new Exception("Model is null!");
-}
-else
-{
-    model = new()
-    {
-        thetas = ThetasInitializations.HeInitialization(layers),
-        normParams = new()
-        {
-            mean = trainingData.Average(),
-            standardDeviation = Tools.GetStandardDeviation(trainingData)
-        }
-    };
-    await Storage.SaveModel(model);
-}
+var trainingData = await EssentialsBuilder.GetTrainingData();
+var model = await EssentialsBuilder.GetModel(layers, trainingData);
 
 var network = new Network(new()
 {
