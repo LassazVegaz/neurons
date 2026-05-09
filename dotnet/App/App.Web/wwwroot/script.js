bfcd9745f092c6a3557ee94a192c6baad6dbd7ec
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-09 16:28:30 +0800

    a minimal slightly working web app

---
+++ dotnet/App/App.Web/wwwroot/script.js
@@ -0,0 +1,17 @@
+﻿window.createChart = (canvasId, data) => {
+    const canvas = document.getElementById(canvasId);
+
+    new Chart(canvas, {
+        type: "line",
+        data
+    });
+}
+
+window.updateChart = (canvasId, data) => {
+    const canvas = document.getElementById(canvasId);
+
+    const chart = Chart.getChart(canvas);
+
+    chart.data = data;
+    chart.update();
+}
