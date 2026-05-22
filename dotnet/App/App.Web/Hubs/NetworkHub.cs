4d670cffeb3a125069c2948eea5f66262498dfa0
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-22 23:41:34 +0800

    basic interfaces and records for Network Hub

--- dotnet/App/App.Web/Hubs/NetworkHub.cs
+++ dotnet/App/App.Web/Hubs/NetworkHub.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
 
 namespace App.Web.Hubs;
 
-public class NetworkHub : Hub
+public record TrainingResult
 {
-    public async Task Train()
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Prediction { get; set; }
+}
+
+public interface INetworkClient
+{
+    Task TrainingFinished(TrainingResult[] results);
+    Task TrainingStopped();
+    Task IterationBreak(int iteration, double[] MSEs);
+}
+
+public record TrainParams
+{
+    public required int[] Layers { get; set; }
+    public bool NewThetas { get; set; }
+    public double Alpha { get; set; }
+    public int Iteration { get; set; }
+}
+
+public class NetworkHub : Hub<INetworkClient>
+{
+    public async Task Train(TrainParams p)
     {
-        Console.WriteLine("Train function");
+        Console.WriteLine($"Train: {ToJson(p)}");
     }
+
+    public async Task StopTraining()
+    {
+        Console.WriteLine("Stop training");
+    }
+
+    private static string ToJson(object o) => JsonSerializer.Serialize(o);
 }
