ff41787c362d0130598e75a13880d853ab7f5b03
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-24 19:19:36 +0800

    normalization class

---
+++ dotnet/App/App.Web/Normalization.cs
@@ -0,0 +1,25 @@
+﻿using Neurons;
+
+namespace App.Web;
+
+public static class Normalization
+{
+    public static NormalizationParameters GetNormalizationParameters(double[] numbers)
+    {
+        var mean = numbers.Average();
+        var sum = numbers.Sum(d => Math.Pow(d - mean, 2));
+        var std = Math.Sqrt(sum / numbers.Length);
+
+        return new()
+        {
+            mean = mean,
+            standardDeviation = std
+        };
+    }
+
+    public static double Normalize(double x, NormalizationParameters p)
+        => (x - p.mean) / p.standardDeviation;
+
+    public static double Denormalize(double x, NormalizationParameters p)
+        => x * p.standardDeviation + p.mean;
+}
