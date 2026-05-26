01ee3be8c6ff222892dd3c5188fdb521c732d5b0
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-27 01:24:30 +0800

    events

--- neurons/QLearning/QLearning.cs
+++ neurons/QLearning/QLearning.cs
@@ -5,18 +5,39 @@ public class QLearning
     private int noOfStates;
     private int noOfActions;
     private double[][] table = [];
+    private CancellationTokenSource? tknCtx;
+
+    public event EventHandler? TrainingStopped;
+    public event EventHandler? TrainingFinished;
 
     public void Train(TrainParameters p)
+    {
+        tknCtx?.Cancel();
+        tknCtx = new();
+
+        Task.Run(() => Train(p, tknCtx.Token));
+    }
+
+
+    private async Task Train(TrainParameters p, CancellationToken token)
     {
         noOfActions = p.noOfActions;
         noOfStates = p.noOfStates;
         table = BuildTable();
 
+        var stopped = false;
         var greediness = 0.0;
         var greedinessRate = 1d / p.iterations;
 
         for (var i = 0; i < p.iterations; i++)
         {
+            if (token.IsCancellationRequested)
+            {
+                stopped = true;
+                TrainingStopped?.Invoke(this, EventArgs.Empty);
+                break;
+            }
+
             greediness += greedinessRate;
             var state = p.initialState;
             var step = -1;
@@ -43,6 +64,8 @@ public class QLearning
                 if (res.gameOver) break;
             }
         }
+
+        if (!stopped) TrainingFinished?.Invoke(this, EventArgs.Empty);
     }
 
     private double[][] BuildTable()
