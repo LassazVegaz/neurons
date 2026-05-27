918637133928d25d29e94b1f56ae3fb0df37adeb
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-28 04:24:15 +0800

    MaxGamesToSend in settings

--- app/server/App.Web/QLearningM/QLearningSettings.cs
+++ app/server/App.Web/QLearningM/QLearningSettings.cs
@@ -4,4 +4,9 @@ public class QLearningSettings
 {
     public int Iterations { get; set; }
     public required string QTableFile { get; set; }
+
+    /// <summary>
+    /// Maximum number of games that can be sent to the client in one training session
+    /// </summary>
+    public int MaxGamesToSend { get; set; }
 }
