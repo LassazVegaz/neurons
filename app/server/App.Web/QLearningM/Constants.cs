5c83ae15920c5f67821e573101e962e1f7303442
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-29 21:15:53 +0800

    ax allowed steps are 18

--- app/server/App.Web/QLearningM/Constants.cs
+++ app/server/App.Web/QLearningM/Constants.cs
@@ -5,7 +5,7 @@ public static class Constants
     /// <summary>
     /// Allowed steps in a game. 0-based
     /// </summary>
-    public const int ALLOWED_STEPS = 9;
+    public const int ALLOWED_STEPS = 17;
 
     public const int STATES = 100;
     public const int ACTIONS = 4;
