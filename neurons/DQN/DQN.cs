171493d7eed4ad14c956498d237b59f7ee8e04ec
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-30 21:21:08 +0800

    refactor

--- neurons/DQN/DQN.cs
+++ neurons/DQN/DQN.cs
@@ -2,25 +2,6 @@
 
 namespace Neurons.DQN;
 
-public class TrainParameters
-{
-    public required Thetas t;
-    public required int iterations;
-    public required int maxPeriods;
-    public required double[] initialState;
-    public required int noOfActions;
-    public required double lambda;
-    public required double alpha;
-    public required Func<double, ActionResults> act;
-}
-
-public class ActionResults
-{
-    public required double reward;
-    public required bool gameOver;
-    public double[] nextState = [];
-}
-
 public class DQN(int[] layers)
 {
     readonly int[] layers = layers;
