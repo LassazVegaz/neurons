7796f598ab3ac54085c621ae84cd5dcffd866388
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-31 15:25:36 +0800

    made it client

--- app/client/src/app/dqn/page.tsx
+++ app/client/src/app/dqn/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { twMerge } from "tailwind-merge";
 import ConnectionDisplay from "@/components/ConnectionDisplay";
 import ControlPanelContainer from "@/components/ControlPanelContainer";
