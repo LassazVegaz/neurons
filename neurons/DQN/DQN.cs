79cae16556bb5d486a4496d20eda59cc7e4e4a74
LassazVegaz <lasindunuwanga@gmail.com>
2026-05-31 16:43:30 +0800

    optimizer

--- neurons/DQN/DQN.cs
+++ neurons/DQN/DQN.cs
@@ -64,12 +64,13 @@ public class DQN()
     {
         noOfActions = p.noOfActions;
         alpha = p.alpha;
-        learningT = p.t.Clone();
+        learningT = p.t;
         act = p.act;
         layers = p.layers;
 
+        var optimizer = new Optimizer(layers, learningT, alpha);
         var learningP = new Predictor(learningT, layers);
-        var targetP = new Predictor(p.t, layers);
+        var targetP = new Predictor(p.t.Clone(), layers);
         var stopped = false;
 
         var greediness = 0;
@@ -106,8 +107,8 @@ public class DQN()
                 if (!actRes.gameOver)
                     targetQ += p.lambda * MaxQ(targetP, actRes.nextState);
 
-                // back propagation
-                Backward(predicted, targetQ, a);
+                // optimize learning thetas
+                optimizer.Optimize(predicted, targetQ, a);
 
                 if (actRes.gameOver) break;
                 s = actRes.nextState;
@@ -120,7 +121,7 @@ public class DQN()
         }
 
         if (!stopped)
-            TrainingFinished?.Invoke(this, targetP.t);
+            TrainingFinished?.Invoke(this, learningT);
     }
 
     private static int GetBestAction(ForwardResults forwardResults)
@@ -132,81 +133,6 @@ public class DQN()
             if (qValues[i] > qValues[maxIdx]) maxIdx = i;
 
         return maxIdx;
-    }
-
-    private void Backward(ForwardResults predicted, double targetQ, int a)
-    {
-        // construct y (targets) for backprop
-        var y = (double[])predicted.befA[^1].Clone();
-        y[a] = targetQ;
-
-        // back propagation and updating thetas
-        var dT = Backward(learningT, predicted, y);
-        UpdateThetas(learningT, dT, alpha);
-    }
-
-    private Thetas Backward(Thetas t, ForwardResults fResult, double[] y)
-    {
-        var dT = ThetasInitializations.ZeroInitialization(layers);
-        var predicted = fResult.befA[^1];
-
-        // error signals comming from every neurone
-        var eSignal = new double[layers.Length][];
-        eSignal[^1] = new double[y.Length];
-        for (var b = 0; b < y.Length; b++)
-            eSignal[^1][b] = -(y[b] - predicted[b]);
-
-        // from the last layer to the first one
-        for (var b = layers.Length - 1; b > 0; b--)
-        {
-            var prevLayerI = b - 1; // prev layer index
-            var gapI = b - 1; // gap index for thetas
-
-            // for every neurone in the current layer
-            for (var c = 0; c < layers[b]; c++)
-            {
-                // accumulating partial derivative of bias
-                dT.b[gapI][c] += eSignal[b][c];
-
-                // for every neurone in the previous layer
-                for (var d = 0; d < layers[prevLayerI]; d++)
-                {
-                    // accumulating partial derivative of weights
-                    dT.w[gapI][c][d] += fResult.a[prevLayerI][d] * eSignal[b][c];
-                }
-            }
-
-            // calculating error signals
-            eSignal[prevLayerI] = new double[layers[prevLayerI]];
-
-            // for every neurone in the previous layer
-            for (var c = 0; c < layers[prevLayerI]; c++)
-            {
-                // for every neurone in the current layer
-                for (var d = 0; d < layers[b]; d++)
-                {
-                    eSignal[prevLayerI][c] +=
-                       Derivatives.DRelU(fResult.befA[prevLayerI][c]) *
-                       t.w[gapI][d][c] * eSignal[b][d];
-                }
-            }
-        }
-
-        return dT;
-    }
-
-    private void UpdateThetas(Thetas t, Thetas dT, double alpha)
-    {
-        for (var b = 0; b < layers.Length - 1; b++)
-        {
-            for (var c = 0; c < layers[b + 1]; c++)
-            {
-                t.b[b][c] -= alpha * dT.b[b][c];
-
-                for (var d = 0; d < layers[b]; d++)
-                    t.w[b][c][d] -= alpha * dT.w[b][c][d];
-            }
-        }
     }
 
     private static double MaxQ(Predictor p, double[] s)
