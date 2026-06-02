194fa5383687d6c3bf40c995512b50ded6f9fd15
LassazVegaz <lasindunuwanga@gmail.com>
2026-06-02 20:26:30 +0800

    simple rewards

--- app/server/App.Web/DQNM/ActionPerformer.cs
+++ app/server/App.Web/DQNM/ActionPerformer.cs
@@ -4,21 +4,20 @@ namespace App.Web.DQNM;
 
 public class ActionPerformer
 {
-    readonly double maxR = Math.Sqrt(0.9 * 0.9 * 2);
-
-
     public ActionResults Act(PeriodContext ctx)
     {
         var x = Math.Round(ctx.currentState[0], 1);
         var y = Math.Round(ctx.currentState[1], 1);
         var a = ctx.actionToTake;
 
+        // + for moving towards the goal
+        var r = (a == 1 && x < 0.9) || (a == 2 && y < 0.9)
+            ? 0.1 : -0.1;
+
         if (a == 0 && y > 0) y -= 0.1;
         else if (a == 1 && x < 0.9) x += 0.1;
         else if (a == 2 && y < 0.9) y += 0.1;
         else if (a == 3 && x > 0) x -= 0.1;
-
-        var r = Math.Sqrt(x * x + y * y) / maxR;
 
         return new()
         {
