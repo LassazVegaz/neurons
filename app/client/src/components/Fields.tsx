371c408db3e154b25b87221e32ae26c975c924a8
LassazVegaz <lasindunuwanga@gmail.com>
2026-06-05 16:02:20 +0800

    fields made better

--- app/client/src/components/Fields.tsx
+++ app/client/src/components/Fields.tsx
@@ -1,53 +1,27 @@
+import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-type TextFieldProps = {
-  label: string;
-  name: string;
-  value: string;
-  onChange: React.ComponentProps<"input">["onChange"];
-};
-
-type CheckboxProps = {
-  label: string;
-  name?: string;
-  checked: boolean;
-  onChange: React.ComponentProps<"input">["onChange"];
-};
-
-export const TextField = (props: TextFieldProps) => (
-  <div className="flex flex-col gap-1">
-    <label htmlFor={props.name}>{props.label}</label>
-    <input
-      name={props.name}
-      type="text"
-      value={props.value}
-      onChange={props.onChange}
-      className="bg-gray-700 text-white rounded px-2 py-1"
-    />
-  </div>
+export const FieldContainer = (props: React.ComponentProps<"div">) => (
+  <div {...props} className={twMerge("flex flex-col gap-1", props.className)} />
 );
 
-export const Checkbox = (props: CheckboxProps) => (
-  <div className="flex items-center gap-2">
-    <input
-      name={props.name}
-      type="checkbox"
-      checked={props.checked}
-      onChange={props.onChange}
-    />
-    <label htmlFor={props.name}>{props.label}</label>
-  </div>
+export const TextField = (props: ComponentProps<"input">) => (
+  <input
+    type="text"
+    {...props}
+    className={twMerge(
+      "bg-gray-700 text-white rounded px-2 py-1",
+      props.className,
+    )}
+  />
 );
 
-export const Button = ({
-  className,
-  ...props
-}: React.ComponentProps<"button">) => (
+export const Button = (props: React.ComponentProps<"button">) => (
   <button
     {...props}
     className={twMerge(
       "text-white font-bold py-2 px-4 rounded disabled:opacity-45 cursor-pointer",
-      className,
+      props.className,
     )}
   />
 );
