990f8b0e49839944b56a8589e11da8377c66419b
LassazVegaz <lasindunuwanga@gmail.com>
2026-06-06 16:12:01 +0800

    bottom section

---
+++ app/client/src/app/dqn/components/BottomSection.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import GameButton from "./GameButton";
+import { GameResults } from "@/signalr/dqn.hub.types";
+import { twMerge } from "tailwind-merge";
+import player from "../helpers/player";
+
+export default function BottomSection() {
+  const [games, setGames] = useState<GameResults[]>([]);
+  const [bestGame, setBestGame] = useState<GameResults | undefined>(undefined);
+  const [currentGame, setCurrentGame] = useState(-1);
+  const [isGamePlaying, setIsGamePlaying] = useState(false);
+
+  const playBestGame = () => {
+    setIsGamePlaying(true);
+    player.playBestGame();
+  };
+
+  const onGamePauseResume = () => {
+    if (isGamePlaying) {
+      player.pause();
+      setIsGamePlaying(false);
+    } else {
+      player.resume();
+      setIsGamePlaying(true);
+    }
+  };
+
+  const playGameFrom = (gameIdx: number) => {
+    setIsGamePlaying(true);
+    player.playFrom(gameIdx);
+  };
+
+  useEffect(() => {
+    player.onGameChange = setCurrentGame;
+
+    return () => {
+      player.onGameChange = undefined;
+    };
+  }, []);
+
+  return (
+    <div className="bg-blue-950 col-span-2 flex flex-col justify-center gap-4 py-4 min-h-20">
+      <div className="flex justify-center items-center gap-4">
+        {games.map((g, idx) => (
+          <GameButton
+            key={g.iteration}
+            isPlayingNow={idx === currentGame}
+            label={g.iteration}
+            rewards={g.totalRewards}
+            onClick={() => playGameFrom(idx)}
+          />
+        ))}
+
+        {bestGame && (
+          <GameButton
+            isPlayingNow={currentGame === games.length}
+            label="Best"
+            rewards={bestGame.totalRewards}
+            onClick={playBestGame}
+            className={twMerge(
+              "bg-red-900",
+              currentGame === games.length && "bg-red-950",
+            )}
+          />
+        )}
+      </div>
+
+      {games.length > 0 && (
+        <div className="flex justify-center">
+          <button
+            className="border border-blue-600 py-2 px-4 rounded cursor-pointer"
+            onClick={onGamePauseResume}
+          >
+            {isGamePlaying ? "Pause" : "Resume"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
--- app/client/src/app/dqn/page.tsx
+++ app/client/src/app/dqn/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 import ConnectionDisplay from "@/components/ConnectionDisplay";
-import ControlPanelContainer from "@/components/ControlPanelContainer";
-import { Checkbox, TextField, Button } from "@/components/Fields";
-import trainingStatusText from "@/constants/training-status-text.constant";
-import TrainingStatus from "@/types/training-status.enum";
-import useUtils from "./helpers/utils.hook";
-import GameButton from "./components/GameButton";
-import { twMerge } from "tailwind-merge";
+import ControlPanel from "./components/ControlPanel";
+import BottomSection from "./components/BottomSection";
 
 const boxes = [] as { id: string }[];
 for (let y = 0; y < 10; y++)
   for (let x = 0; x < 10; x++) boxes.push({ id: `box-${x}-${y}` });
 
 export default function DQNPage() {
-  const utils = useUtils();
-
   return (
     <>
       <div className="q-learning h-full grid grid-cols-[1fr_300px] grid-rows-[1fr_auto]">
@@ -30,137 +23,9 @@ export default function DQNPage() {
           </div>
         </div>
 
-        <ControlPanelContainer className="border-l border-blue-400">
-          <Checkbox
-            checked={utils.autoPlay}
-            label="Auto Play"
-            onChange={utils.onAutoPlayChange}
-          />
-          <Checkbox
-            checked={utils.form.noGreedy}
-            label="No Greedy"
-            name="noGreedy"
-            onChange={utils.onFieldChange}
-          />
-          <Checkbox
-            checked={utils.form.createNewThetas}
-            label="New table"
-            name="createNewThetas"
-            onChange={utils.onFieldChange}
-          />
-          <TextField
-            label="Iterations"
-            name="iterations"
-            value={utils.form.iterations}
-            onChange={utils.onFieldChange}
-          />
-          <TextField
-            label="Alpha"
-            name="alpha"
-            value={utils.form.alpha}
-            onChange={utils.onFieldChange}
-          />
-          <TextField
-            label="Lambda"
-            name="lambda"
-            value={utils.form.lambda}
-            onChange={utils.onFieldChange}
-          />
-          <TextField
-            label="Layers"
-            name="layers"
-            value={utils.form.layers}
-            onChange={utils.onFieldChange}
-          />
+        <ControlPanel />
 
-          <div className="flex gap-2 items-end">
-            <TextField
-              label="Speed"
-              name="speed"
-              value={utils.form.speed}
-              onChange={utils.onFieldChange}
-            />
-            <button
-              className="border p-1 px-3 rounded border-blue-600 cursor-pointer"
-              onClick={utils.onChangeSpeedButtonCLick}
-            >
-              Set
-            </button>
-          </div>
-
-          <div className="h-10" />
-
-          {(utils.status === TrainingStatus.Finished ||
-            utils.status === TrainingStatus.NotStarted ||
-            utils.status === TrainingStatus.Stopped) && (
-            <>
-              <Button
-                className="border border-blue-600"
-                onClick={utils.onTrainClick}
-              >
-                Train
-              </Button>
-              <Button
-                className="border border-purple-600"
-                onClick={utils.getTheBestGameClick}
-              >
-                Best Game
-              </Button>
-            </>
-          )}
-          {utils.status === TrainingStatus.InProgress && (
-            <Button
-              className="border border-red-600"
-              onClick={utils.onStopClick}
-            >
-              Stop
-            </Button>
-          )}
-
-          <div>
-            Status: {trainingStatusText[utils.status]}
-            {utils.status === TrainingStatus.InProgress &&
-              utils.trainingCompletion}
-          </div>
-        </ControlPanelContainer>
-
-        <div className="bg-blue-950 col-span-2 flex flex-col justify-center gap-4 py-4 min-h-20">
-          <div className="flex justify-center items-center gap-4">
-            {utils.games.map((g, idx) => (
-              <GameButton
-                key={g.iteration}
-                isPlayingNow={idx === utils.currentGame}
-                label={g.iteration}
-                rewards={g.totalRewards}
-                onClick={() => utils.playGameFrom(idx)}
-              />
-            ))}
-
-            {utils.bestGame && (
-              <GameButton
-                isPlayingNow={utils.currentGame === utils.games.length}
-                label="Best"
-                rewards={utils.bestGame.totalRewards}
-                onClick={utils.playBestGame}
-                className={twMerge(
-                  "bg-red-900",
-                  utils.currentGame === utils.games.length && "bg-red-950",
-                )}
-              />
-            )}
-          </div>
-
-          {utils.games.length > 0 && (
-            <div className="flex justify-center">
-              <button
-                className="border border-blue-600 py-2 px-4 rounded cursor-pointer"
-                onClick={utils.onGamePauseResume}
-              >
-                {utils.isGamePlaying ? "Pause" : "Resume"}
-              </button>
-            </div>
-          )}
-        </div>
+        <BottomSection />
       </div>
 
       <ConnectionDisplay hide />
