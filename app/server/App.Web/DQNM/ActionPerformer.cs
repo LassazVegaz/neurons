796451a98b2de1c5a0ebf4cedb68e2edd714e14c
LassazVegaz <lasindunuwanga@gmail.com>
2026-06-08 02:24:13 +0800

    better accuracy with doubles time is 0-1

--- app/server/App.Web/DQNM/ActionPerformer.cs
+++ app/server/App.Web/DQNM/ActionPerformer.cs
@@ -5,6 +5,8 @@ namespace App.Web.DQNM;
 public class ActionPerformer
 {
     private const int MAX_PERIODS_0BASED = Constants.MAX_PERIODS - 1;
+    private const double TIME_RATE = 1.0 / MAX_PERIODS_0BASED;
+    private const double EPS = 1e-6;
 
     public ActionResults Act(PeriodContext ctx)
     {
@@ -26,13 +28,19 @@ public class ActionPerformer
         else if (a == 2 && pY < 0.9) pY += 0.1;
         else if (a == 3 && pX > 0) pX -= 0.1;
 
-        t++;
+        pX = Math.Round(pX, 1);
+        pY = Math.Round(pY, 1);
+        oX = Math.Round(oX, 1);
+        oY = Math.Round(oY, 1);
+
+        t += TIME_RATE;
+        if (Math.Abs(1 - t) < EPS) t = 1.0;
 
         // then rewards for new states
         var r = 0.0;
-        if (pX == oX && pY == oY)
+        if (Math.Abs(pX - oX) < EPS && Math.Abs(pY - oY) < EPS)
             r = -1;
-        else if (t == MAX_PERIODS_0BASED)
+        else if (t >= 1 - EPS)
             r = 1;
 
         return new()
